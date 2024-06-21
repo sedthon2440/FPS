a593b13b10122318c796a0c3dcccce014233e6ed
BiLaL <97440505+E9N99@users.noreply.github.com>
2024-06-21 04:46:39 +0300

    Update __init__.py

--- SedUb/sql_helper/__init__.py
+++ SedUb/sql_helper/__init__.py
@@ -8,21 +8,19 @@ from ..Config import Config
 from ..core.logger import logging
 
 LOGS = logging.getLogger(__name__)
+engine = None
+BASE = declarative_base()
 
 def start() -> scoped_session:
+    global engine
     engine = create_engine(Config.DB_URI)
     BASE.metadata.bind = engine
     BASE.metadata.create_all(engine)
     return scoped_session(sessionmaker(bind=engine, autoflush=False))
 
-
 try:
-    BASE = declarative_base()
     SESSION = start()
 except AttributeError as e:
     # this is a dirty way for the work-around required for #23
-    LOGS.error(
-        "DB_URI is not configured. Features depending on the database might have issues."
-    )
+    LOGS.error("DB_URI is not configured. Features depending on the database might not work.")
     LOGS.error(str(e))
-
